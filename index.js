b5707c072fc5217f7fbf6af72cf1b55b3b1da580
Matias Peñas <mattff3@gmail.com>
2020-10-24 22:07:38 -0300

    Adding events inside one connection

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-  
-  io.on('connection', (socket) => {
+    
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
-  });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    
+});
 
 http.listen(3001, () => {
   console.log('listening on *:3001');
